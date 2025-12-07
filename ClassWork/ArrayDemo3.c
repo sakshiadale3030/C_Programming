1b756fcda39874b4496578f9bb2648f6b2223a94
sakshiadale3030 <sakshiadale30@gmail.com>
2025-12-07 15:50:10 +0530

    ClassWork

---
+++ ClassWork/ArrayDemo3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+   int Arr[4] = {10,20,30,40};
+
+   printf("Arr : %lu\n",Arr);
+   printf("&Arr : %lu\n",&Arr);
+   printf("&(Arr[0]) : %lu\n",&(Arr));
+ return 0;
+}
